06bae0cf83b2018f5efa7b5b2f7738e253e47e71
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-08-16 08:19:27 +0000

    test external repo New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,hrdb,fonts,neon,en.json,neon] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,lib,logos,momentLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md] Removed files - [fonts,material]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "neon",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestProj_16Aug",
+  "homePage" : "Main",
+  "name" : "TestProj_16Aug",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
